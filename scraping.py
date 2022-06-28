05c910fb0eec12d9b7fa5881f866b970bef52581
Sergio Gómez Conde <58828599+SergioGomezConde@users.noreply.github.com>
2022-06-28 18:44:02 +0200

    Obtención de códigos y enlaces de asignaturas

--- scraping.py
+++ scraping.py
@@ -61,9 +61,9 @@ i = 0
 # Almacenamiento de la informacion en el fichero JSON
 for elemento in elementos:
     nombre_asignatura = elemento.text.split(' (')[0].capitalize()
-    codigo_asignatura = elemento.text.split(' (')[1]
+    codigo_asignatura = elemento.text.split(' (')
 #     .split(')').split('-')[3]
-    print(codigo_asignatura)
+    print(len(codigo_asignatura))
 #     enlace_asignatura = elemento.get_attribute('href')
 #     if (nombre_asignatura != "") and ("Grado en" not in nombre_asignatura):
     if (nombre_asignatura != ""):
