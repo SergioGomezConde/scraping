a6fba31a57b5363c25860c4f0c11cf13044becb7
serggom <sergio.s.gomezconde@gmail.com>
2022-07-02 02:26:14 +0200

    Avances con la obtención de eventos

--- scraping.py
+++ scraping.py
@@ -238,7 +238,9 @@ driver.get('https://campusvirtual.uva.es/calendar/export.php?')
 
 driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/form/div[2]/div[2]/fieldset/div/label[1]/input').click()
 driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/form/div[3]/div[2]/fieldset/div/label[5]/input').click()
+
 time.sleep(2)
+
 driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/form/div[4]/div[2]/fieldset/div/div[2]/span/input').click()
 
 with open(ficheroJSON, 'w') as ficheroDatosJSON:
