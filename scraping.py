792aa2bef2a2d712fe5e14a890486ea12e550247
serggom <sergio.s.gomezconde@gmail.com>
2022-07-03 02:02:16 +0200

    Obtención del número de participantes

--- scraping.py
+++ scraping.py
@@ -94,12 +94,14 @@ for elemento in elementos:
             'porcentaje': porcentaje_asignatura,
             'plan': plan_asignatura,
             'codigo': codigo_asignatura,
-            'numero_participantes': numero_participantes,
+            # 'numero_participantes': numero_participantes,
             'enlace': enlace_asignatura,
             'enlace_participantes': enlace_participantes
         })
 
         i = i + 1
+
+        print(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div[3]/form/div/p'))
 
         driver.close() # BORRAR
 
