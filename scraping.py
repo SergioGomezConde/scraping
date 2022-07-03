4d36a6643819ca96cc40d703e971bceb446a708d
serggom <sergio.s.gomezconde@gmail.com>
2022-07-03 02:54:13 +0200

    Obtención de eventos

--- scraping.py
+++ scraping.py
@@ -167,6 +167,7 @@ driver.find_element(by=By.XPATH,
 
 time.sleep(2)
 
+
 driver.find_element(by=By.XPATH,
                     value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/form/div[4]/div[2]/fieldset/div/'
                           'div[2]/span/input').click()
