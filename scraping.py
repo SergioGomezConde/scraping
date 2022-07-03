ee4af2dd63676fc93e941f162ea5137c9dfd7d8f
serggom <sergio.s.gomezconde@gmail.com>
2022-07-03 02:42:30 +0200

    Avances

--- scraping.py
+++ scraping.py
@@ -161,6 +161,7 @@ contenidoJSON['numero_mensajes'].append({
 
 driver.get('https://campusvirtual.uva.es/calendar/export.php?')
 
+driver.implicitly_wait(5)
 driver.find_element(by=By.XPATH,
                     value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/form/div[2]/div[2]/fieldset/div/'
                           'label[1]/input').click()
